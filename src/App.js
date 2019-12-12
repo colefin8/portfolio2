54c2f03d4dd379f9b986c13eaf7e9a419799472a
Cole Finlayson <cole.fin8@gmail.com>
2019-12-12 15:34:52 -0700

    getting there

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
 import {
   makeStyles,
   createMuiTheme,
+  useTheme,
   ThemeProvider
 } from "@material-ui/core/styles";
 import {
   Table,
   Tab,
   Tabs,
-  TabPanel,
+  Typography,
   Drawer,
   Container,
   Paper
@@ -17,13 +19,6 @@ import { amber, blueGrey } from "@material-ui/core/colors";
 
 import "./App.css";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: blueGrey,
-    secondary: amber
-  }
-});
-
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
@@ -31,17 +26,45 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles(theme => ({
-  drawer: {
-    height: "100vh",
-    width: "20vw"
-  },
-  drawerPaper: {},
-  drawerTabs: {},
-  tabs: {
-    borderRight: `1px solid ${theme.palette.secondary}`
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey,
+    secondary: amber
   }
-}));
+});
+
+const useStyles = makeStyles(() => {
+  console.log(theme);
+  return {
+    drawer: {},
+    drawerPaper: {
+      height: "100vh",
+      width: "20vw"
+    },
+    drawerTabs: {},
+    tabs: {
+      borderRight: `1px solid ${theme.palette.secondary}`
+    },
+    container: {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
+    },
+    paper1: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText
+    },
+    paper2: {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText
+    },
+    paper3: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText
+    }
+  };
+});
 
 function App() {
   const classes = useStyles();
@@ -52,24 +75,81 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Drawer
-        anchor="left"
-        open={true}
-        variant="persistent"
-        classes={{ paper: classes.drawerPaper }}
-        className={classes.drawer}
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
       >
-        <Tabs className={classes.tabs} orientation="vertical">
-          <Tab label="About" {...a11yProps(0)} />
-          <Tab label="Skills" {...a11yProps(0)} />
-          <Tab label="Projects" {...a11yProps(0)} />
-        </Tabs>
-      </Drawer>
-      <Container component="article" maxWidth="lg">
-        <Paper>About</Paper>
-        <Paper>Skills</Paper>
-        <Paper>Projects</Paper>
-      </Container>
+        <Drawer
+          anchor="left"
+          open={true}
+          variant="persistent"
+          classes={{ paper: classes.drawerPaper }}
+          className={classes.drawer}
+        >
+          <Tabs
+            className={classes.tabs}
+            orientation="vertical"
+            onChange={handleChange}
+            value={value}
+          >
+            <Tab label="About" {...a11yProps(0)} />
+            <Tab label="Skills" {...a11yProps(1)} />
+            <Tab label="Projects" {...a11yProps(2)} />
+          </Tabs>
+        </Drawer>
+        <div style={{ width: "80vw" }}>
+          <Container
+            component="article"
+            maxWidth="lg"
+            classes={{ root: classes.container }}
+          >
+            <Paper
+              square={true}
+              elevation={1}
+              classes={{
+                elevation1: classes.paper1,
+                elevation2: classes.paper2,
+                elevation3: classes.paper3
+              }}
+            >
+              <Typography color="inherit">
+                {"I'm a full stack web developer from Provo, UT"}
+              </Typography>
+            </Paper>
+            <Paper
+              square={true}
+              elevation={2}
+              classes={{
+                elevation1: classes.paper1,
+                elevation2: classes.paper2,
+                elevation3: classes.paper3
+              }}
+            >
+              <Table></Table>
+            </Paper>
+            <Paper
+              square={true}
+              elevation={3}
+              classes={{
+                elevation1: classes.paper1,
+                elevation2: classes.paper2,
+                elevation3: classes.paper3
+              }}
+            >
+              <Carousel autoPlay={false} indicators={true} animation="slide">
+                <Typography color="inherit">Listo</Typography>
+                <Typography color="inherit">LoveToKnow</Typography>
+                <Typography color="inherit">CardSmart</Typography>
+              </Carousel>
+            </Paper>
+          </Container>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
